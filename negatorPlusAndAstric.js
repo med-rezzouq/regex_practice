aceb0e0c1fb65e3a00c6a8014e854d5ad3cb318c
rezzouq <mohammed.rezzouq@siway.ma>
2022-10-04 13:25:40 +0100

    ci(difference between the + and *):

--- negatorPlusAndAstric.js
+++ negatorPlusAndAstric.js
@@ -1,15 +1,19 @@
 let myWord = "word monsieur 3.6298774 word worrrrrd";
+let str = "over the moon";
 
 let fullregx = /[^2-6w-z]/g; //this will search each caracter that is not between 2-6 and w-z and put it inside array result
 
 let reg = /[r+]/g; //this will search each caracter or manyoccurence of it
 
-let rem = /[oerf+]/g; //this will search each caracter or manyoccurence of it
-let ref = /[oerf*]/g; //this will search each caracter o 0 or many
+let rem = /or+/; //this will search each caracter or manyoccurence of it
+let ref = /g*o*/; //this will search each caracter o 0 or many
+let req = /go*/; //this will search each caracter o 0 or many
 
 console.log(myWord.match(fullregx));
 console.log(myWord.match(reg));
-console.log(myWord.match(ref));
+console.log(myWord.match(rem));
+console.log(str.match(ref));
+console.log(str.match(req));
 
 //this is also the same thing using new RegExp()
 // console.log(myWord.match(new RegExp(fullregx)));
